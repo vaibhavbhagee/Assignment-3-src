7fb68439ae15aaf85d6c918f48048a47fd010063
aayan636 <aayan.kumar123@gmail.com>
2016-04-22 15:56:52 +0530

    separate file to launch game

--- Ion/SWING Tests/MouseClickOrMotion.java
+++ Ion/SWING Tests/MouseClickOrMotion.java
@@ -277,13 +277,16 @@ System.out.println(board_side);
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         //javax.swing.SwingUtilities.invokeLater(new Runnable() {
         //    public void run() {
-                createAndShowGUI();
+                //createAndShowGUI();
         //    }
         //});
+    //}
+    public static void launch(){
+        createAndShowGUI();
     }
 }
